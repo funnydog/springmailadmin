0af9c80d43d98a018385524c50d4c9d200139500
funnydog <funnydog@users.noreply.github.com>
2024-06-18 08:02:35 +0200

    Replace the initial passwords with bcrypt hashes.

--- src/main/resources/data-hsqldb.sql
+++ src/main/resources/data-hsqldb.sql
@@ -1,13 +1,17 @@
--- authentication data
+-- authentication data (username = 'user', password = 'password')
 INSERT INTO users(username,password,enabled) VALUES ('user','$2a$10$XRKlq21QvHTnR2hDMptn6u8W9/AxA00upBpU4qd.fJsR2ROH4u2RO', TRUE);
 INSERT INTO authorities(username,authority) VALUES('user','ROLE_USER');
 
 -- application data
 INSERT INTO domain(id, name,description,active,backup) VALUES (1, 'google.com','Main google domain',TRUE,FALSE);
-INSERT INTO mailbox(domain_id,email,password,active) VALUES(1,'clauper@google.com', 'pwd1', TRUE);
-INSERT INTO mailbox(domain_id,email,password,active) VALUES(1,'gstefani@google.com', 'pwd2', TRUE);
-INSERT INTO mailbox(domain_id,email,password,active) VALUES(1,'dbowie@google.com', 'pwd3', TRUE);
-INSERT INTO mailbox(domain_id,email,password,active) VALUES(1,'fmercury@google.com', 'pwd4', TRUE);
+-- password = pwd1
+INSERT INTO mailbox(domain_id,email,password,active) VALUES(1,'clauper@google.com', '$2a$10$An2kX7cIv3SSbjx2/iE61OycYJ5ce.4j8XKevunJuaOHp5RGic.ba', TRUE);
+-- password = pwd2
+INSERT INTO mailbox(domain_id,email,password,active) VALUES(1,'gstefani@google.com', '$2a$10$jjT/kqFAbQvzUWduzM04Lual0NcOuZucl0LvtOu7GPtORP81Ru9Yq', TRUE);
+-- password = pwd3
+INSERT INTO mailbox(domain_id,email,password,active) VALUES(1,'dbowie@google.com', '$2a$10$3giLHuuZ/8L2eVxpG9jNWOFFYwn8mF5BnNM3lMzjnxBs3WK1C9yJ2', TRUE);
+-- password = pwd4
+INSERT INTO mailbox(domain_id,email,password,active) VALUES(1,'fmercury@google.com', '$2a$10$k1MpB95JoNZFl4QrU9Aef.lHU0UsXQXzLgv9w8/EZz5RnYAjK8Dky', TRUE);
 INSERT INTO alias(domain_id,destination,redirect,active) VALUES(1,'nodoubt@google.com','gstefani@google.com', TRUE);
 INSERT INTO alias(domain_id,destination,redirect,active) VALUES(1,'queen@google.com','fmercury@google.com', TRUE);
 
